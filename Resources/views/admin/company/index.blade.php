1049bc75d5903a79554c01aca0559f1812783427
Tayeb-Ali <Elteyab.Hassan@gmail.com>
2020-10-06 11:36:03 +0100

    complete company lang field add .gitignore

--- Resources/views/admin/company/index.blade.php
+++ Resources/views/admin/company/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin::layouts.content')
 
 @section('page_title')
-{{ __('organization::app.company.title') }}
+    {{ __('organization::app.company.title') }}
 @stop
 
 
@@ -20,81 +20,103 @@
         </div>
 
         <div class="page-content">
-          <div class="table">
-            <div class="grid-container">
+            <div class="table">
+                <div class="grid-container">
 
-              <div class="filter-row-one" id="datagrid-filters">
+                    <div class="filter-row-one" id="datagrid-filters">
 
-                <div class="search-filter">
-                  <form method="GET" action="{{ route('admin.company.index') }}" accept-charset="UTF-8" class="" style="display: contents;" role="search">
-                    <input type="search" id="search-field" name="search" class="control" placeholder="{{ __('ui::app.datagrid.search') }}" />
-                    <div class="icon-wrapper">
-                        <button class="icon search-icon search-btn" type="submit" style="border: 0px; background-color: inherit;"></button>
-                    </div>
-                  </form>
-                </div>
-
-                <div class="dropdown-filters per-page">
-                  <form method="GET" action="{{ url()->current() }}">
-                    <div class="control-group">
-                        <label class="per-page-label" for="perPage">
-                            {{ __('ui::app.datagrid.items-per-page') }}
-                        </label>
-                        <select id="perPage" name="perPage" class="control" onchange="this.form.submit();">
-                          <option value="10"{{ request()->get('perPage') == 10 ? ' selected' : '' }}> 10 </option>
-                          <option value="20"{{ request()->get('perPage') == 20 ? ' selected' : '' }}> 20 </option>
-                          <option value="30"{{ request()->get('perPage') == 30 ? ' selected' : '' }}> 30 </option>
-                          <option value="40"{{ request()->get('perPage') == 40 ? ' selected' : '' }}> 40 </option>
-                          <option value="50"{{ request()->get('perPage') == 50 ? ' selected' : '' }}> 50 </option>
-                        </select>
-                    </div>
-                  </form>
-                </div>
-
-              </div>
-
-              <div class="filter-row-two">
-              </div>
-
-              <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th><th>{{ trans('organization::app.company.fields.description') }}</th><th>{{ trans('organization::app.company.fields.currency') }}</th><th>{{ trans('organization::app.company.fields.status') }}</th><th>Actions</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                @foreach($company as $item)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $item->description }}</td><td>{{ $item->currency }}</td><td>{{ $item->status }}</td>
-                        <td class="actions" style="white-space: nowrap; width: 100px;">
-                          <div class="action">
-                            <a href="{{ route('admin.company.show', $item->company_id) }}" title="View Company">
-                              <button style="border: none; background-color: inherit;"><span class="icon eye-icon"></span></button>
-                            </a>
-                            <a href="{{ route('admin.company.edit', $item->company_id) }}" title="Edit Company">
-                              <button style="border: none; background-color: inherit;"><span class="icon pencil-lg-icon"></span></button>
-                            </a>
-
-                            <form method="POST" action="{{ route('admin.company.delete', $item->company_id) }}" accept-charset="UTF-8" style="display:inline">
-                                {{ method_field('DELETE') }}
-                                {{ csrf_field() }}
-                                <button type="submit" style="border: none; background-color: inherit;" title="Delete Company" onclick="return confirm(&quot;Confirm delete?&quot;)">
-                                  <span class="icon trash-icon"></span>
-                                </button>
+                        <div class="search-filter">
+                            <form method="GET" action="{{ route('admin.company.index') }}" accept-charset="UTF-8"
+                                  class="" style="display: contents;" role="search">
+                                <input type="search" id="search-field" name="search" class="control"
+                                       placeholder="{{ __('ui::app.datagrid.search') }}"/>
+                                <div class="icon-wrapper">
+                                    <button class="icon search-icon search-btn" type="submit"
+                                            style="border: 0px; background-color: inherit;"></button>
+                                </div>
                             </form>
-                          </div>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
+                        </div>
 
-              </table>
-              <div class="pagination"> {!! $company->appends(['search' => Request::get('search')])->render() !!} </div>
+                        <div class="dropdown-filters per-page">
+                            <form method="GET" action="{{ url()->current() }}">
+                                <div class="control-group">
+                                    <label class="per-page-label" for="perPage">
+                                        {{ __('ui::app.datagrid.items-per-page') }}
+                                    </label>
+                                    <select id="perPage" name="perPage" class="control" onchange="this.form.submit();">
+                                        <option value="10"{{ request()->get('perPage') == 10 ? ' selected' : '' }}> 10
+                                        </option>
+                                        <option value="20"{{ request()->get('perPage') == 20 ? ' selected' : '' }}> 20
+                                        </option>
+                                        <option value="30"{{ request()->get('perPage') == 30 ? ' selected' : '' }}> 30
+                                        </option>
+                                        <option value="40"{{ request()->get('perPage') == 40 ? ' selected' : '' }}> 40
+                                        </option>
+                                        <option value="50"{{ request()->get('perPage') == 50 ? ' selected' : '' }}> 50
+                                        </option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
 
+                    </div>
+
+                    <div class="filter-row-two">
+                    </div>
+
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>{{ trans('organization::app.company.fields.description') }}</th>
+                            <th>{{ trans('organization::app.company.fields.currency') }}</th>
+                            <th>{{ trans('organization::app.company.fields.status') }}</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @foreach($company as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $item->description }}</td>
+                                <td>{{ $item->currency }}</td>
+                                <td>{{ $item->status }}</td>
+                                <td class="actions" style="white-space: nowrap; width: 100px;">
+                                    <div class="action">
+                                        <a href="{{ route('admin.company.show', $item->company_id) }}"
+                                           title="View Company">
+                                            <button style="border: none; background-color: inherit;"><span
+                                                        class="icon eye-icon"></span></button>
+                                        </a>
+                                        <a href="{{ route('admin.company.edit', $item->company_id) }}"
+                                           title="Edit Company">
+                                            <button style="border: none; background-color: inherit;"><span
+                                                        class="icon pencil-lg-icon"></span></button>
+                                        </a>
+
+                                        <form method="POST"
+                                              action="{{ route('admin.company.delete', $item->company_id) }}"
+                                              accept-charset="UTF-8" style="display:inline">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                            <button type="submit" style="border: none; background-color: inherit;"
+                                                    title="Delete Company"
+                                                    onclick="return confirm(&quot;Confirm delete?&quot;)">
+                                                <span class="icon trash-icon"></span>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+
+                    </table>
+                    <div class="pagination"> {!! $company->appends(['search' => Request::get('search')])->render() !!} </div>
+
+                </div>
             </div>
-          </div>
         </div>
     </div>
 
--- Resources/views/admin/company/show.blade.php
+++ Resources/views/admin/company/show.blade.php
@@ -1,7 +1,7 @@
 @extends('admin::layouts.content')
 
 @section('page_title')
-{{ __('organization::app.company.show') }}
+    {{ __('organization::app.company.show') }}
 @stop
 
 
@@ -10,37 +10,55 @@
     <div class="content">
         <div class="page-header">
             <div class="page-title">
-                <i class="icon angle-left-icon back-link" onclick="history.length > 1 ? history.go(-1) : window.location = '{{ route('admin.dashboard.index') }}';"></i>
+                <i class="icon angle-left-icon back-link"
+                   onclick="history.length > 1 ? history.go(-1) : window.location = '{{ route('admin.dashboard.index') }}';"></i>
                 <h1>{{ __('organization::app.company.title') }}</h1>
             </div>
             <div class="page-action">
                 <a href="{{ route('admin.company.edit', $company->company_id) }}" title="Edit Company">
-                  <button style="border: none; background-color: inherit;"><span class="icon pencil-lg-icon"></span></button>
+                    <button style="border: none; background-color: inherit;"><span class="icon pencil-lg-icon"></span>
+                    </button>
                 </a>
-                <form method="POST" action="{{ route('admin.company.delete', $company->company_id) }}" accept-charset="UTF-8" style="display:inline">
+                <form method="POST" action="{{ route('admin.company.delete', $company->company_id) }}"
+                      accept-charset="UTF-8" style="display:inline">
                     {{ method_field('DELETE') }}
                     {{ csrf_field() }}
-                    <button type="submit" style="border: none; background-color: inherit;" title="Delete Company" onclick="return confirm(&quot;Confirm delete?&quot;)">
-                      <span class="icon trash-icon"></span>
+                    <button type="submit" style="border: none; background-color: inherit;" title="Delete Company"
+                            onclick="return confirm(&quot;Confirm delete?&quot;)">
+                        <span class="icon trash-icon"></span>
                     </button>
                 </form>
             </div>
         </div>
 
         <div class="page-content">
-          <div class="table">
+            <div class="table">
 
-            <table class="table">
-                <tbody>
+                <table class="table">
+                    <tbody>
                     <tr>
                         <td>ID</td>
                         <td>{{ $company->company_id }}</td>
                     </tr>
-                    <tr><td> {{ trans('organization::app.company.fields.description') }} </td><td> {{ $company->description }} </td></tr>\n<tr><td> {{ trans('organization::app.company.fields.currency') }} </td><td> {{ $company->currency }} </td></tr>\n<tr><td> {{ trans('organization::app.company.fields.status') }} </td><td> {{ $company->status }} </td></tr>\n
-                </tbody>
-            </table>
+                    <tr>
+                        <td> {{ trans('organization::app.company.fields.description') }} </td>
+                        <td> {{ $company->description }} </td>
+                    </tr>
+                    \n
+                    <tr>
+                        <td> {{ trans('organization::app.company.fields.currency') }} </td>
+                        <td> {{ $company->currency }} </td>
+                    </tr>
+                    \n
+                    <tr>
+                        <td> {{ trans('organization::app.company.fields.status') }} </td>
+                        <td> {{ $company->status }} </td>
+                    </tr>
+                    \n
+                    </tbody>
+                </table>
 
-          </div>
+            </div>
         </div>
     </div>
 @stop
