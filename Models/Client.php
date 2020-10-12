79a5249886296da398f57608012a0fe341cd695b
Tayeb-Ali <Elteyab.Hassan@gmail.com>
2020-10-13 00:09:58 +0100

    try to fix non-Error fix  by solve load querELequent wen use with=[]

--- Models/Client.php
+++ Models/Client.php
@@ -16,10 +16,10 @@ class Client extends Model
 //     protected $with = ['company', 'group'];
 
     /**
-    * The database primary key value.
-    *
-    * @var string
-    */
+     * The database primary key value.
+     *
+     * @var string
+     */
     protected $primaryKey = 'client_id';
 
     /**
@@ -28,18 +28,17 @@ class Client extends Model
      * @var array
      */
     protected $fillable = ['company_id', 'group_id', 'cust_type', 'name', 'contact_person', 'phone', 'fax', 'pobox', 'email', 'country', 'city', 'address', 'have_branch', 'status', 'credit_limit', 'begin_bal_credit', 'begin_bal_debit', 'curr_bal_credit', 'curr_bal_debit', 'amend_by', 'amend_date', 'acc_mgr', 'account_code', 'last_trns_date', 'last_trns_value', 'last_trns_type', 'begin_bal_credit_fc', 'begin_bal_debit_fc', 'curr_bal_credit_fc', 'curr_bal_debit_fc', 'currency_code', 'allowance_days', 'mship_to_address', 'mship_to_phone', 'mship_to_fax', 'mship_to_city', 'mship_to_pobox', 'online_cust', 'analysis_code', 'sprice_list', 'vat_flag'];
+
 //
-//    public function group()
-//    {
-//        return $this->belongsTo(ClientGroup::class, 'group_id', 'group_id');
-//    }
-//
-//      public function company()
-//      {
-//          return $this->belongsTo(Company::class, 'company_id', 'company_id');
-//      }
+    public function group()
+    {
+        return $this->belongsTo(ClientGroup::class, 'group_id', 'group_id');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id', 'company_id');
+    }
 
 
-
-    
 }
--- Models/EmployGroup.php
+++ Models/EmployGroup.php
@@ -30,16 +30,15 @@ class EmployGroup extends Model
      */
     protected $fillable = ['company_id', 'group_id', 'group_desc', 'account_code', 'status', 'amend_by', 'amend_date'];
 
-//    public function employ()
-//    {
-//        return $this->hasMany(Employ::class, 'group_id', 'group_id');
-//    }
+    public function employ()
+    {
+        return $this->hasMany(Employ::class, 'group_id', 'group_id');
+    }
 
-//    public function company()
-//    {
-//        return $this->hasOne(Company::class, 'company_id', 'company_id');
-//    }
+    public function company()
+    {
+        return $this->hasOne(Company::class, 'company_id', 'company_id');
+    }
 
 
-
-}
+    }
