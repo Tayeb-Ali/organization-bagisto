89b8ac637ae0262ac62da23271409bdee0f6b515
Tayeb-Ali <Elteyab.Hassan@gmail.com>
2020-11-01 10:08:17 +0200

    ADD migration File

---
+++ Database/Migrations/2020_11_01_1604217330_create_erp_stores_groups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateErpStoresGroupsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('erp_stores_groups', function (Blueprint $table) {
+
+		$table->increments('group_id');
+            $table->string('company_code',10);
+            $table->string('group_code',15);
+            $table->string('group_desc',100)->nullable();
+            $table->string('group_desc_o',100)->nullable();
+            $table->string('cost_center_code',30)->nullable();
+            $table->string('status',10)->nullable();
+            $table->string('amend_by',10)->nullable();
+            $table->datetime('amend_date')->nullable();
+            $table->primary(['company_code','group_code']);
+            $table->unsignedInteger('company_id')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('erp_stores_groups');
+    }
+}
---
+++ Database/Migrations/2020_11_01_1604217350_create_erp_stores_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateErpStoresTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('erp_stores', function (Blueprint $table) {
+
+            $table->increments('stores_id', 10);
+            $table->string('company_code', 10);
+            $table->string('code', 10);
+            $table->string('description', 100)->nullable();
+            $table->string('name_o', 100)->nullable();
+            $table->string('store_keeper', 100)->nullable();
+            $table->string('store_group', 10)->nullable();
+            $table->string('status', 10)->nullable();
+            $table->string('amend_by', 10)->nullable();
+            $table->datetime('amend_date')->nullable();
+            $table->datetime('last_trns_date')->nullable();
+            $table->decimal('last_trns_value', 13, 3)->nullable();
+            $table->string('last_trns_type', 100)->nullable();
+            $table->string('cost_center_code', 30)->nullable();
+            $table->string('have_sub_stores', 1)->nullable();
+            $table->string('store_contacts', 50)->nullable();
+            $table->string('store_location', 500)->nullable();
+            $table->tinyinteger('sub_store_mandatory', 4)->nullable();
+            $table->string('account_code', 30)->nullable();
+            $table->string('dept_code', 10)->nullable();
+            $table->primary(['company_code', 'code']);
+            $table->unsignedInteger('company_id')->nullable();
+            $table->unsignedInteger('group_id')->nullable();
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('erp_stores');
+    }
+}
