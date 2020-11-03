97cd50f3a7278ac75d221ad5b9dc147a5845b918
Tayeb-Ali <Elteyab.Hassan@gmail.com>
2020-11-03 15:34:17 +0200

    update #14

--- Resources/views/admin/employ/form.blade.php
+++ Resources/views/admin/employ/form.blade.php
@@ -20,7 +20,19 @@
             </div>
         </div>
         <div class="row">
-            @include('organization::admin.helper.company-dropdown')
+            <div class="control-group col col-6 {{ $errors->has('company_id') ? 'has-error' : ''}}">
+                <label for="company_id"
+                       class="required">{{ __('organization::app.employ.fields.company_id') }}</label>
+
+                <select id="company_id" name="company_id" class="form-control control" data-role="control">
+                    @foreach($company as $item)
+                        <option value="{{ $item->company_id }}" {{$employ->company_id == $item->company_id ? 'selected' : '' }}>{{ $item->description }}</option>
+                    @endforeach
+                </select>
+
+                {!! $errors->first('company_id', '<span class="control-error">:message</span>') !!}
+            </div>
+
             <div class="control-group col col-6 {{ $errors->has('group_id') ? 'has-error' : ''}}">
                 <label for="company_id" class="required">{{ __('organization::app.employ.fields.group_id') }}</label>
 
