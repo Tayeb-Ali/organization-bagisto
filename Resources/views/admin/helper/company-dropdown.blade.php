4ca50c15bca1ad5249c75b0088025f724725707e
Tayeb-Ali <Elteyab.Hassan@gmail.com>
2020-11-03 15:32:44 +0200

    update #14

--- Resources/views/admin/helper/company-dropdown.blade.php
+++ Resources/views/admin/helper/company-dropdown.blade.php
@@ -1,7 +1,7 @@
 <div class="control-group {{ $errors->has('company_id') ? 'has-error' : ''}}">
     <label for="company_id" class="required">{{ __('organization::app.employ.fields.company_id') }}</label>
 
-    <select id="company_id" name="company_id" class="form-control" data-role="control">
+    <select id="company_id" name="company_id" class="form-control control" data-role="control">
 
         @foreach($company as $item)
             <option value="{{ $item->company_id }}" {{$employ->company_id == $item->company_id ? 'selected' : '' }}>{{ $item->description }}</option>
